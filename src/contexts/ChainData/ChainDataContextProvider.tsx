b5274874484dd463a0af90b861d41cf79df7765a
Kai Pham <duykhangpham201@gmail.com>
2022-02-21 17:09:02 -0500

    Chain Data Context Finished

--- src/contexts/ChainData/ChainDataContextProvider.tsx
+++ src/contexts/ChainData/ChainDataContextProvider.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ChainDataContext from './ChainDataContext'
+import useWallet from 'hooks/useWallet'
+import { MAINNET_CHAIN_DATA, ChainData, POLYGON_CHAIN_DATA } from 'utils/connectors'
 
 const ChainDataContextProvider: React.FC = ({ children }) => {
+    const [chain, setChain] = useState<ChainData>(MAINNET_CHAIN_DATA)
+    const { account, ethereum, isMetamaskConnected } = useWallet()
+
+    const setMainnet = () => {
+        setChain(MAINNET_CHAIN_DATA)
+        if (isMetamaskConnected)
+            ethereum?.send('wallet_switchEthereumChain', [
+                { chainId: "0x1" },
+                account,
+            ])
+    }
+
+    const setPolygon = () => {
+        setChain(POLYGON_CHAIN_DATA)
+        if (isMetamaskConnected)
+            ethereum?.send('wallet_addEthereumChain', [
+                {
+                    chainId: "0x89",
+                    chainName: "Polygon",
+                    nativeCurrency: {
+                        name: 'Matic',
+                        symbol: 'MATIC',
+                        decimals: 18,
+                    },
+                    rpcUrls: ['https://rpc-mainnet.maticvigil.com'],
+                    blockExplorerUrls: ['https://polygonscan.com/']
+                },
+                account,
+            ])
+    }
+
     return (
         <ChainDataContext.Provider
-            value={{}}
+            value={{
+                chain,
+                setMainnet,
+                setPolygon
+            }}
         >
             {children}
         </ChainDataContext.Provider>

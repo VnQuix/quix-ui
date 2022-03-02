28a8621d409d577ff1aec3402c8be6acc306a5e2
Kai Pham <duykhangpham201@gmail.com>
2022-03-01 19:27:33 -0500

    Successfully Interact with Aave Smart Contract

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -1,6 +1,2 @@
-export const aaveDebtAdapterContract =
-  "0xeB9EabF3BbACbd057a6980E80bB627FF9183b7f6";
-export const aaveTokenAdapterContract =
-  "0x15Dd5058daDefa5449968dBD6C36563d1BB9b3c8";
-export const aaveInteractiveAdapterContract =
-  "0x8e7D259c1FD2fCd0094D44ee6eCCe560aFAFB37a";
+export const aaveAdapterContractAddress =
+  "0x86ceb1ed190de05c473d92c35882a28a8a112f91";
--- src/contexts/Save/SaveContextProvider.tsx
+++ src/contexts/Save/SaveContextProvider.tsx
@@ -1,33 +1,26 @@
 import React from 'react'
 
 import SaveContext from './SaveContext'
-import { aaveTokenAdapterContract } from "constants/contractAddresses"
-import AaveTokenAdapterAbi from "abi/AaveTokenAdapter.json"
+import { aaveAdapterContractAddress } from "constants/contractAddresses"
+import AaveAdapterAbi from "abi/AaveAdapter.json"
 import useWallet from 'hooks/useWallet';
 import { getContract } from 'utils/contractHelpers';
 import { AbiItem } from "web3-utils";
-import ethers from "ethers"
-import Web3 from "web3";
 
 const SaveContextProvider: React.FC = ({ children }) => {
-    const { ethereum } = useWallet()
+    const { ethereum, account } = useWallet()
+
     const AaveTokenAdapter = getContract(
         ethereum,
-        aaveTokenAdapterContract,
-        AaveTokenAdapterAbi.abi as unknown as AbiItem
+        aaveAdapterContractAddress,
+        AaveAdapterAbi.abi as unknown as AbiItem
     )
     const getBalance = async () => {
-
-        const web3 = new Web3(ethereum)
-        const contract = new web3.eth.Contract(
-            AaveTokenAdapterAbi.abi as unknown as AbiItem,
-            aaveTokenAdapterContract
-        )
         try {
-            const balance = await contract.methods.getName("0x13512979ADE267AB5100878E2e0f485B568328a4").call()
+            const balance = await AaveTokenAdapter.methods.getCollateralInEth(account).call()
             return balance
         } catch (e) {
-            return "none"
+            return "Error"
         }
     }
 
